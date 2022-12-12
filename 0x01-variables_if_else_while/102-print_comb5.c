fa9aaffbad126ecf07313ec0a0a25e005c78db25
yonatanmulugeta <yonatanmulugeta93@gmail.com>
2022-12-12 11:11:39 -0800

    task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - describes if n is less than 5, greater than 6 or 0.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i,count=48;
+	putchar((count )+'0');
+	return(0);
+}
