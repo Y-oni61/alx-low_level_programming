d6b543ab94ef06ebe4ab3778059b05c2e9dfa0cd
yonatanmulugeta <yonatanmulugeta93@gmail.com>
2023-01-12 09:34:10 -0800

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,11 +10,9 @@
  * Return: Always(0) Success
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	while (argc--)
-	{
-		printf("%s\n", *argv++);
-	}
+	printf("%s\n", argv[0]);
+	(void)argc;
 	return (0);
 
